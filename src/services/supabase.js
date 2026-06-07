8ef7128d195db3d2f10b550625bfcd2b91e8b817
Muhammad Shees <mohammadshees2004@gmail.com>
2026-06-07 23:52:14 +0500

    supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const SUPABASE_URL = 'https://eogevrmzyquwpslivxfk.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_36UqRL__625yv2Y0Y2Grmw_YBnoTfiU';
+const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const SUPABASE_KEY = process.env.EXPO_PUBLIC_SUPABASE_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
     auth: {
